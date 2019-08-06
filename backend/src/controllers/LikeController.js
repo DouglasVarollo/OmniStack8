6df8cbf15a2a4ad71d52e2698573bec1636f7b19
Douglas Varollo <dcvarollo@gmail.com>
2019-08-06 17:33:16 -0300

    back-end finalizado

---
+++ backend/src/controllers/LikeController.js
@@ -0,0 +1,25 @@
+const Dev = require("../models/Dev");
+
+module.exports = {
+  async store(req, res) {
+    const { user } = req.headers;
+    const { devId } = req.params;
+
+    const loggeDev = await Dev.findById(user);
+    const targetDev = await Dev.findById(devId);
+
+    if (!targetDev) {
+      return res.status(400).json({ error: "Dev not exists" });
+    }
+
+    if (targetDev.likes.includes(loggeDev._id)) {
+      console.log("Deu match");
+    }
+
+    loggeDev.likes.push(targetDev._id);
+
+    await loggeDev.save();
+
+    return res.send(loggeDev);
+  }
+};
